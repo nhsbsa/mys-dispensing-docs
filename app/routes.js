e323b520d201a8ca03490fa36afb56e466d5781c
jolumley <49235579+jolumley@users.noreply.github.com>
2021-01-14 11:36:43 +0000

    fp34d

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,20 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+
+// dispensing docs routes //
+router.post('/fp34d/v1/vaccine-add-another', function(req,res) {
+
+    var addAnother = req.session.data['add-another']
+
+    if (addAnother == 'yes') {
+        res.redirect('/fp34d/v1/doctor-details')
+    }
+    else if(addAnother == 'submit') {
+        res.redirect('/optometry/ssecs/form/declare-submit')
+    }
+    else {
+        res.redirect('/fp34d/v1/start-page')
+    }
+})
 module.exports = router;
