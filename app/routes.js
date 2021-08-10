f99d9a80fb6e21fb64f43df98e338610c7d30c33
Douglas Caven <douglas.caven@nhs.net>
2021-08-10 15:02:05 +0100

    Doug

--- app/routes.js
+++ app/routes.js
@@ -153,7 +153,7 @@ router.get(/addAnotherDate/, function (req, res) {
   if (req.query.radioInlineGroup === "Yes" ) {
     res.redirect('lft-start-day'); // if both input values are "Yes" - redirect to 'page-name' //
   } else {
-    res.redirect('declaration-lft'); // if both input values are "Yes" - redirect to 'page-name' //
+    res.redirect('declaration2'); // if both input values are "Yes" - redirect to 'page-name' //
   }
 });
 
